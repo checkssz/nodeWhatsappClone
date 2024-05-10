b8f02c1c539238b5edda28e820b05fb88149b5e7
Matheus Victorio <matheusvic2016@gmail.com>
2024-05-10 01:53:39 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const port = process.env.port || 4000;
